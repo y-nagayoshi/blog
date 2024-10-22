8b58b2194419c5c76e08a8424fb66d20a4954ac4
y-nagayoshi <wakasagi135@gmail.com>
2024-10-23 04:43:43 +0900

    ブログ一覧ページ、詳細ページ追加

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.conf import settings
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return(
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'DRAFT'
@@ -24,6 +30,9 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
+    
+    objects = models.Manager()
+    published = PublishedManager()
     
     class Meta:
         ordering = ['-publish']
